caf3558927b3237997169bcfe679041319c18f6c
kst <kst@osstem.com>
2023-07-26 18:01:12 +0900

    fix : 기능 추가

--- src/main/java/kr/co/galaxy/user/application/dto/LoginRequest.java
+++ src/main/java/kr/co/galaxy/user/application/dto/LoginRequest.java
@@ -1,5 +1,7 @@
 package kr.co.galaxy.user.application.dto;
 
 public class LoginRequest {
+    private String email;
+    private String password;
 
 }
--- src/main/java/kr/co/galaxy/user/auth/token/JwtProvider.java
+++ src/main/java/kr/co/galaxy/user/auth/token/JwtProvider.java
@@ -1,11 +1,18 @@
 package kr.co.galaxy.user.auth.token;
 
+import io.jsonwebtoken.Jwts;
+import java.util.Date;
+import lombok.RequiredArgsConstructor;
+
 public class JwtProvider {
 
     private String secretKey;
 
     public String create(){
-        return null;
+        return Jwts.builder()
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime() + 999999999))
+                .compact();
     }
 
     public boolean validation(){
