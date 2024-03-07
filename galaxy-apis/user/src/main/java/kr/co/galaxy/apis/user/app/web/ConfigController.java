4f492ff3140b4b5e47442e33fdf2fc5a7326b6d0
kst <kst@osstem.com>
2024-03-07 19:55:05 +0900

    fix : config server 설정

---
+++ galaxy-apis/user/src/main/java/kr/co/galaxy/apis/user/app/web/ConfigController.java
@@ -0,0 +1,45 @@
+package kr.co.galaxy.apis.user.app.web;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class Description 
+ *
+ *
+ * @class ConfigController
+ * @author SungTae Kim
+ * @version 1.0
+ * @modification
+ * <pre>
+ * since	    author	        description
+ * ---------	------------	---------------------
+ * 2024-03-07      SungTae Kim	    최초 작성		
+ * </pre>
+ */
+
+@RestController
+@RequestMapping("config")
+public class ConfigController {
+
+    @Value("${galaxy.use}")
+    private boolean use;
+
+    @Value("${galaxy.key}")
+    private String value;
+
+    @GetMapping
+    public ResponseEntity<Object> get(){
+        List<Object> result = new ArrayList<>();
+        result.add(use);
+        result.add(value);
+        return ResponseEntity.ok(result);
+    }
+}
